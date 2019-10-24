3c828e828b1e1904994cfcd0cb9f0f40d35ea7aa
Ahmad Nurazhim <ahmadfatihnurazhim@gmail.com>
2019-10-24 07:25:11 +0700

    another cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 require('dotenv').config();
+
+const 
 
 app.use(cors());
 
